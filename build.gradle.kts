1a97a51a06a79995552ce8fdd00044b038cbf644
Emma Böcker <stckoverflw@gmail.com>
2022-06-02 17:36:36 +0200

    add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("multiplatform") version "1.6.21"
+    `maven-publish`
 }
 
 group = "net.stckoverflw"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -35,5 +36,49 @@ tasks {
     task<GenerateTask>("generateWordsFile") {
         words.set(file("words.txt").readLines().filterNot { it.startsWith('#') })
         output.set(file("src/commonGenerated/").toPath())
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl("https://maven.stckoverflw.net/releases")
+
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+        }
+    }
+
+    publications {
+        filterIsInstance<MavenPublication>().forEach { publication ->
+            publication.pom {
+                name.set(project.name)
+                description.set("ratio + don't care, in your kotlin code")
+                url.set("https://github.com/StckOverflw/ratio-kt")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/StckOverflw/ratio-kt/blob/main/LICENSE")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("Emma Böcker")
+                        email.set("stckoverflw@gmail.com")
+                        organizationUrl.set("https://www.stckoverflw.net")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/StckOverflw/ratio-kt.git")
+                    developerConnection.set("scm:git:https://github.com/StckOverflw/ratio-kt.git")
+                    url.set("https://github.com/StckOverflw/ratio-kt")
+                }
+            }
+        }
     }
 }
